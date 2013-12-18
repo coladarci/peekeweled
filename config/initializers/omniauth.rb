6c2c7c8b3da47aa099ffdb22b4c84501ed60ccf8
Greg Coladarci <coladarci@gmail.com>
2013-12-17 22:37:12 -0500

    authentication done via twitter

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,12 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  #I know this is bad - but for this demo, I'm not inclined to protect my keys w/ ENV vars :)
+  provider :twitter, "2oiy64m06NiTIRlHFS4DQ", "5qzvu2tkCzkImYApx4guAbS59FYQlO7RyujBNju4",
+    {
+      :secure_image_url => 'true',
+      :image_size => 'original',
+      :authorize_params => {
+        :force_login => 'true',
+        :lang => 'pt'
+      }
+    }
+end
